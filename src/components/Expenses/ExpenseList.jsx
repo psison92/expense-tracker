1fe7701fec5fd2780e520746d5bc5a0ec86bd69e
psison <sisonpatrick29@yahoo.com>
2022-11-01 15:40:44 -0500

    add filter by year to ExpenseList

--- src/components/Expenses/ExpenseList.jsx
+++ src/components/Expenses/ExpenseList.jsx
@@ -1,31 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './ExpenseList.css'
 import ExpenseItem from "./ExpenseItem"
+import ExpensesFilter from './ExpensesFilter'
 
 const ExpenseList = props => {
+  const [filteredYear, setFilteredYear] = useState('2020')
+
+  const filterChangeHandler = selectedYear => {
+    setFilteredYear(selectedYear)
+  }
+
+  const filteredExpenses = props.expenses.filter(expense => {
+    return expense.date.getFullYear().toString() === filteredYear
+  })
+
+  let expensesContent = <p>No expenses found.</p>
+
+  if (filteredExpenses.length > 0) {
+    expensesContent = filteredExpenses.map(expense =>  
+      <ExpenseItem 
+        key={expense.id}
+        title={expense.title} 
+        amount={expense.amount} 
+        date={expense.date}
+      />
+    )
+  }
 
   return (  
-    <div className="expenses">
-      <ExpenseItem
-        title={props.expenses[0].title}
-        amount={props.expenses[0].amount}
-        date={props.expenses[0].date}
-      />
-      <ExpenseItem
-        title={props.expenses[1].title}
-        amount={props.expenses[1].amount}
-        date={props.expenses[1].date}
-      />
-      <ExpenseItem
-        title={props.expenses[2].title}
-        amount={props.expenses[2].amount}
-        date={props.expenses[2].date}
-      />
-      <ExpenseItem
-        title={props.expenses[3].title}
-        amount={props.expenses[3].amount}
-        date={props.expenses[3].date}
-      />
+    <div>
+    
+      <div className="expenses">
+        <ExpensesFilter 
+          onChangeFilter={filterChangeHandler} 
+          selected={filteredYear}  
+        />
+        {expensesContent}
+      </div>
     </div>
   )
 }
